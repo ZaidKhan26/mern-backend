14e96c7dfd443a632d7154b9170f5bf634f70d8e
ZaidKhan26 <zzzaaaiiiddd044@gmail.com>
2025-08-04 18:33:22 +0530

    Fix: use dynamic port from environment for Render

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const applicantRoutes = require('./routes/applicantRoutes');
+const PORT = process.env.PORT || 5000;
+
 
 dotenv.config();
 const app = express();
@@ -17,8 +19,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(5000, () => {
-      console.log('Server is running on http://localhost:5000');
+    app.listen(PORT, () => {
+      console.log('Server is running on port ${PORT}');
     });
   })
   .catch((error) => console.error(error));
